a618617a5e1a15310fb41131ce67bba61ad93c0e
Hong-Wen Huang <gogoraiser@gmail.com>
2024-10-12 14:17:27 +0800

    Initialize Spring Boot Project

---
+++ src/main/java/com/owen2077/springbootmalldemo/SpringbootMallDemoApplication.java
@@ -0,0 +1,13 @@
+package com.owen2077.springbootmalldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owen2077/springbootmalldemo/SpringbootMallDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen2077.springbootmalldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
